6e8bbca948b0c45e5369e6087eed10293f118643
LINCC <916727397@qq.com>
2026-05-03 14:28:31 +0800

    Refine FAB menu label and motion polish

--- fab-lib/src/main/java/com/github/clans/fab/FloatingActionMenu.java
+++ fab-lib/src/main/java/com/github/clans/fab/FloatingActionMenu.java
@@ -613,8 +613,8 @@ public class FloatingActionMenu extends ViewGroup {
             int left = mLabelsPaddingLeft;
             int top = mLabelsPaddingTop;
             if (mLabelsShowShadow) {
-                left += fab.getShadowRadius() + Math.abs(fab.getShadowXOffset());
-                top += fab.getShadowRadius() + Math.abs(fab.getShadowYOffset());
+                left += label.calculateShadowWidth();
+                top += label.calculateShadowHeight();
             }
 
             label.setPadding(
@@ -822,7 +822,7 @@ public class FloatingActionMenu extends ViewGroup {
 
                             Label label = (Label) fab.getTag(R.id.fab_label);
                             if (label != null && label.isHandleVisibilityChanges()) {
-                                label.hide(false);
+                                label.hide(animate);
                             }
                         }
                     }, delay);
